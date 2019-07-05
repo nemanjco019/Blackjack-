ef3fc137d518535a66e6328876e02c0e37c35ed3
Nemanja <cokojevicnemanja@gmail.com>
2019-07-05 15:39:18 +0200

    Game logic started

--- src/blackjack/Blackjack.java
+++ src/blackjack/Blackjack.java
@@ -1,4 +1,7 @@
 package blackjack;
+
+import java.util.Scanner;
+
 public class Blackjack {
     public static void main(String[] args) {
         
@@ -12,13 +15,57 @@ public class Blackjack {
         //Create a deck for the player
         Deck playerDeck = new Deck();
         
-        //Create a deck for second player
-        Deck playerDeck2 = new Deck();
-        
         //Create a deck for the dealer 
         Deck dealerDeck = new Deck();
         
-        System.out.println(playingDeck);
+        double playerMoney = 100.00;
+        
+        Scanner userInput = new Scanner(System.in);
+        
+        //Gaming logic
+        while(playerMoney > 0){
+            //Game in progress
+            System.out.println("You have " + playerMoney + ", how much would you like to bet?");
+            double playerBet = userInput.nextDouble();
+            if(playerBet > playerMoney){
+                System.out.println("You can't bet that amount");
+                break;
+            }
+            
+            //Start dealing cards 
+            playerDeck.draw(playingDeck);
+            playerDeck.draw(playingDeck);
+            
+            dealerDeck.draw(playingDeck);
+            dealerDeck.draw(playingDeck);
+            
+            while(true){
+                
+                //Player's hand
+                System.out.println("Your hand: ");
+                System.out.println(playerDeck.toString());
+                System.out.println("Your deck has a value of: " + playerDeck.cardsValue());
+                
+                //Dealer's hand
+                System.out.println("Dealer hand: " + dealerDeck.getCard(0).toString() + " and [Hidden]");
+                
+                System.out.println("Would you like to Hit(1) or Stand(2)");
+                int response = userInput.nextInt();
+                
+                if(response == 1){
+                    playerDeck.draw(playingDeck);
+                    System.out.println("You draw a: " + playerDeck.getCard(playerDeck.deckSize()-1).toString());
+                        if(playerDeck.cardsValue() > 21){
+                            System.out.println("Busted ! Your hand is valued at: " + playerDeck.cardsValue());
+                            playerMoney -= playerBet;
+                            
+                        }
+                    
+                }
+            }
+        }
+       //Game over
+        System.out.println("Game is over, you lost all money.");
     }
     
 }
--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -49,6 +49,43 @@ public class Deck {
         this.cards.add(addCard);
     }
     
+    public int deckSize(){
+        return this.cards.size();
+    }
+    
+    public int cardsValue(){
+        int totalValue = 0;
+        int aces = 0;
+        
+        for(Card aCard : this.cards){
+            switch(aCard.getValue()){
+                case TWO: totalValue += 2; break;
+                case THREE: totalValue += 3; break;
+                case FOUR: totalValue += 4; break;
+                case FIVE: totalValue += 5; break;
+                case SIX: totalValue += 6; break;
+                case SEVEN: totalValue += 7; break;
+                case EIGHT: totalValue += 8; break;
+                case NINE: totalValue += 9; break;
+                case TEN: totalValue += 10; break;
+                case JACK: totalValue += 10; break;
+                case QUEEN: totalValue += 10; break;
+                case KING: totalValue += 10; break;
+                case ACE: aces += 1; break;
+            }          
+        }
+            
+        for(int i =0; i < aces; i++){
+            if (totalValue > 10){
+                totalValue += 1;
+            }
+            else{
+                totalValue += 11;
+            }
+        }
+        return totalValue;
+    }
+    
     public String toString(){
         String cardListOutput = "";     
         for (Card aCard : this.cards){
